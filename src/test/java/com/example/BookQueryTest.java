d62310e06d27afc1c3ecc1c41dc7e1692ee9d4b6
Uragami Taichi <backpaper0@gmail.com>
2021-07-08 08:29:37 +0900

    GraphQLの機能を試す

--- src/test/java/com/example/BookQueryTest.java
+++ src/test/java/com/example/BookQueryTest.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -41,6 +43,59 @@ public class BookQueryTest {
 				.path("book.author.name")
 				.entity(String.class)
 				.isEqualTo("鳥山 明");
+	}
 
+	// https://graphql.org/learn/queries/#aliases
+	@Test
+	void aliases() {
+		String query = "{" +
+				"  book1: book(isbn: \"4-08-851181-6\") {" +
+				"    title" +
+				"  }" +
+				"  book2: book(isbn: \"4-08-851831-4\") {" +
+				"    title" +
+				"  }" +
+				"}";
+
+		graphQlTester.query(query)
+				.execute()
+
+				.path("book1")
+				.entity(Map.class)
+				.isEqualTo(Map.of("title", "Dr.スランプ 1"))
+
+				.path("book2")
+				.entity(Map.class)
+				.isEqualTo(Map.of("title", "DRAGON BALL 1"));
+	}
+
+	// https://graphql.org/learn/queries/#fragments
+	@Test
+	void fragments() throws Exception {
+		String query = "{" +
+				"  book1: book(isbn: \"4-08-851181-6\") {" +
+				"    ...bookFields" +
+				"  }" +
+				"  book2: book(isbn: \"4-08-851831-4\") {" +
+				"    ...bookFields" +
+				"  }" +
+				"}" +
+				"fragment bookFields on Book {" +
+				"  title" +
+				"  author {" +
+				"    name" +
+				"  }" +
+				"}";
+
+		graphQlTester.query(query)
+				.execute()
+
+				.path("book1")
+				.entity(Map.class)
+				.isEqualTo(Map.of("title", "Dr.スランプ 1", "author", Map.of("name", "鳥山 明")))
+
+				.path("book2")
+				.entity(Map.class)
+				.isEqualTo(Map.of("title", "DRAGON BALL 1", "author", Map.of("name", "鳥山 明")));
 	}
 }
