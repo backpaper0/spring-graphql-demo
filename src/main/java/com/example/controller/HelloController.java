7f783b1290672e2697889975ff9fc776656f12d0
Uragami Taichi <backpaper0@gmail.com>
2021-07-10 22:06:41 +0900

    Web MVCのコントローラーも追加できる

---
+++ src/main/java/com/example/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public Object get() {
+		return Map.of("message", "Hello, world!");
+	}
+}
